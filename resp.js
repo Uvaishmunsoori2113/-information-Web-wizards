9adf8230720fc90bc0edc7bab01adee76f00d22d
Uvaishmunsoori2113 <140684085+Uvaishmunsoori2113@users.noreply.github.com>
2024-10-16 01:08:07 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,22 @@
+// burger = document.querySelector('.burger');
+// navbar = document.querySelector('.navbar');
+// navlist = document.querySelector('.navlist');
+// link = document.querySelector('.link');
+// logo = document.querySelector('.logo');
+
+
+
+// burger.addEventListener('click', () => {
+//     navbar.classList.toggle('h-nav-rsp');
+//     navlist.classList.toggle('v-class-rsp');
+//     link.classList.toggle('v-class-rsp');
+//     logo.classList.toggle('v-class-rsp');
+
+
+// })
+
+
+
+// j query cdn
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
